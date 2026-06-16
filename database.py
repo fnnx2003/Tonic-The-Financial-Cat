e699aab81784a6defbd6a24e5b1953a56c06dec5
fnnx2003 <fnnx2003@gmail.com>
2026-06-16 22:45:47 +0300

    database fix

--- database.py
+++ database.py
@@ -1,26 +1,23 @@
-from sqlalchemy.orm import Session, scoped_session
-from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy import create_engine, engine
 from models import User, Transactions, init_db
-username = input("Enter a username:\n")
-email = input("Enter your Email:\n")
-password = input("Enter a password:\n")
-initail_balance = float(input("Enter your starting balance:\n"))
-def get_user():
-    with Session(init_db) as session:
-        with session.begin():
-            user_id = User.id
-            user_username = User(username=username)
-            session.add(user_username)
-            user_email = User(email=email)
-            session.add(user_email)
-            user_password = User(password=password)
-            session.add(user_password)
-            user_initial_balance = User(initail_balance=initail_balance)
-            session.add(user_initial_balance)
-db = scoped_session(get_user)
-engine = create_engine(init_db, echo=True)
-get_user.metadata.create_all(engine)
-result = get_user.query(User).all()
+create_engine("sqlite:///tonic_finance.db", echo=True)
+sessionlocal = sessionmaker(bind=engine)
 if __name__ == "__main__":
-    get_user
-    print(result)
+    username = input("Enter a username:\n")
+    email = input("Enter your Email:\n")
+    password = input("Enter a password:\n")
+    initial_balance = float(input("Enter your starting balance:\n"))
+def create_user(username, email, password, initial_balance):
+    with sessionlocal() as session:
+        with session.begin():
+            new_user = User(username=username, email=email, password=password, initial_balance=initial_balance, nullable=False)
+            session.add(new_user)
+            session.commit()
+db = scoped_session(sessionlocal)
+def result():
+    with sessionlocal() as session:
+        users = session.query(User).all()
+        for user in users:
+            print(user)
+print(result())
