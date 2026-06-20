62d9e62a94a2c56f4f1466d5e6ad5cddca761191
fnnx2003 <fnnx2003@gmail.com>
2026-06-20 16:56:16 +0300

    .db removed from git

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from sqlalchemy import create_engine, engine
 from models import User, Transactions, init_db
-create_engine("sqlite:///tonic_finance.db", echo=True)
+engine = create_engine("sqlite:///tonic_finance.db", echo=True)
 sessionlocal = sessionmaker(bind=engine)
 if __name__ == "__main__":
     username = input("Enter a username:\n")
@@ -11,13 +11,19 @@ if __name__ == "__main__":
 def create_user(username, email, password, initial_balance):
     with sessionlocal() as session:
         with session.begin():
-            new_user = User(username=username, email=email, password=password, initial_balance=initial_balance, nullable=False)
+            new_user = User(username=username, email=email, password=password, initial_balance=initial_balance)
             session.add(new_user)
             session.commit()
 db = scoped_session(sessionlocal)
+def create_transtacion(type, amount, category, description, date):
+    with sessionlocal() as session:
+        with session.begin:
+            new_transaction = Transactions(type=type, amount=amount, category=category, description=description, date=date)
+            session.add(new_transaction)
+            session.commit()
 def result():
     with sessionlocal() as session:
-        users = session.query(User).all()
-        for user in users:
-            print(user)
+        user_data = session.query(User).all()
+        print(f"the {user_data} is")
+create_user(username, email, password, initial_balance)
 print(result())
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
@@ -9,21 +10,19 @@ class User(base):
     username = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(225), nullable=False)
-    initial_balance = Column(Float, default=0.0)
-    
-    trasactions = relationship("Transaction", back_populates="user", cascade="all, delete-orphan")
+    initial_balance = Column(Decimal, default=0.0)
+    transactions = relationship("Transactions", back_populates="user", cascade="all, delete-orphan")
     def __repr__(self):
         return f"<User(username='{self.username}', email='{self.email}')>"
 class Transactions(base):
-    __tablename__ = "Transacions"
+    __tablename__ = "Transactions"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     type = Column(String(10), nullable=False)
-    amount = Column(Float, nullable=False)
+    amount = Column(Decimal, nullable=False)
     category = Column(String(50), nullable=False)
     description = Column(String(225))
     date = Column(DateTime, default=datetime.utcnow)
-
     user = relationship("User", back_populates="transactions")
     def __repr__(self):
         return f"<Transactions(types='self.type', amount='self.amount', category='self.category')>"
